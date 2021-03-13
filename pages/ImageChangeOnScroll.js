b35aa628229a76fc1b6f64dcef7cd01588f96b6d
Raul Castro <raulc27@gmail.com>
2021-03-12 23:28:51 -0300

    ultimas modificacoes

--- pages/ImageChangeOnScroll.js
+++ pages/ImageChangeOnScroll.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import ImageToggleOnScroll from '../src/ImageToggleOnScroll';
 
+
+
+
+
 const ImageChangeOnScroll = () => {
-    return(
-       <div>
-           {
-               [1,2,3,4,5,6,7,8].map((imgId)=>{
-                  return( 
-                    <div key={imgId}>
-                       <ImageToggleOnScroll
-                       primaryImg={`/static/imagem${imgId}.jpg`}
-                       secondaryImg={`/static/imagem${imgId}.jpg`}
-                        alt=""
-                       />
-                   </div>
-                  );
-               })
-           }
-       </div>
+
+    const [currentImageId, setCurrentImageId] = useState(0);
+    const [mouseEventCnt, setMouseEventCnt] = useState(0);
+
+    useEffect(() => {
+        window.document.title = `imgId: ${currentImageId}`;
+        console.log(`useEffect: setting title to ${currentImageId}`);
+    },[currentImageId]);
+
+
+
+    return (
+        <div>
+            <span>mouseEventCnt: ${mouseEventCnt}</span>
+            {
+                [1, 2, 3, 4, 5, 6, 7, 8].map((imgId) => {
+                    return (
+                        <div key={imgId}>
+                            <ImageToggleOnScroll
+                                primaryImg={`/static/imagem${imgId}.jpg`}
+                                secondaryImg={`/static/imagem${imgId}.jpg`}
+                                alt=""
+                            />
+                        </div>
+                    );
+                })
+            }
+        </div>
     );
 };
 
